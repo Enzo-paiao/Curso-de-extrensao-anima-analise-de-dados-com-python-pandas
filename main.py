66185c8eb332facb2233c7b40b088a073109dd02
Enzo Moreira Paião <82907775+Enzo-paiao@users.noreply.github.com>
2023-02-07 22:14:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 
-combustiveis_df = pd.read_csv("ca-2021-02.csv")
+combustiveis_df = pd.read_excell("ca-2021-02.xlsx")
 ca_df = combustiveis_df[['Revenda', 'Municipio','Produto', 'Valor de Venda' ]]
 print(combustiveis_df.describe())
 print(combustiveis_df.info())
